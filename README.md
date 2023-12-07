a942d7a3dbcadfe28548d562322d4ac0d777b534
Yuzhuo Wu <48614196+Vision0220@users.noreply.github.com>
2023-12-07 19:25:09 +0800

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # HR650X-IPMI-Auto-Fan
+
 Use Python to automatically set fan speed for Lenovo HR650X Server
+
+
+This should also works for HR630X and some other Lenovo servers with the same ipmi raw code.
+
+# Requirements
+
+1. install ipmitool on your system
+2. install pyyaml: `pip install pyyaml`
+
+# Usage
+
+```bath
+python ipmi_manager.py
+```
+
+
+Tested on Python 3.8
+
+
+# TODO
+
+1. wrap these codes into a docker container
+2. support multiple devices
---
+++ ipmi_manager.py
@@ -0,0 +1,83 @@
+'''
+Author: Yuzhuo Wu
+Date: 2023-12-07 19:19:28
+LastEditTime: 2023-12-07 19:19:56
+LastEditors: Yuzhuo Wu
+Description: 
+FilePath: \HR650X-IPMI-Auto-Fan\ipmi_manager.py
+'''
+import subprocess
+import re
+import yaml
+import time
+import datetime
+
+def get_timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def get_temperature(ipmi):
+    cmd = f"ipmitool -I lanplus -H {ipmi['host']} -U {ipmi['username']} -P '{ipmi['password']}' sensor | grep CPU | grep Temp"
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    if process.returncode != 0:
+        print(f"Error executing command: {cmd}. Error: {error}")
+        return None
+
+    output = output.decode('utf-8')
+    lines = output.split('\n')
+    temperatures = []
+
+    for line in lines:
+        if 'Temp' in line:
+            temp = re.findall(r'\d+\.\d+', line)
+            if temp:
+                temperatures.append(float(temp[0]))
+
+    if not temperatures:
+        print("No temperature data found.")
+        return None
+
+    return max(temperatures)
+
+
+def set_fan_speed(speed, ipmi):
+    cmd = f"ipmitool -I lanplus -H {ipmi['host']} -U {ipmi['username']} -P '{ipmi['password']}' raw 0x2e 0x30 00 00 {speed}"
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    if process.returncode != 0:
+        print(f"Error executing command: {cmd}. Error: {error}")
+        return False
+
+    return True
+
+
+def get_fan_speed(temp, fan_speeds):
+    for fan_speed in fan_speeds:
+        if fan_speed['temp_range'][0] <= temp < fan_speed['temp_range'][1]:
+            return fan_speed['speed']
+    return 100
+
+
+def main():
+    with open('HR650X.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    temp = get_temperature(config['ipmi'])
+
+    if temp is None:
+        print("No temperature data found.")
+        return
+
+    speed = get_fan_speed(temp, config['fan_speeds'])
+
+    if set_fan_speed(speed, config['ipmi']):
+        print(f"Set fan speed to {speed}% for CPU temperature {temp}°C")
+
+
+if __name__ == "__main__":
+    while True:
+        print(get_timestamp(), end=' ')
+        main()
+        time.sleep(10)
